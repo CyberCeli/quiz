84cd450496c8309e407b20e5605afc24aeba3b94
CyberCeli <Aracelihair@gmail.com>
2021-07-14 13:16:59 -0600

    api

--- quiz-app/src/Components/TriviaData.jsx
+++ quiz-app/src/Components/TriviaData.jsx
@@ -1,4 +1,6 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
+
 
 
 
@@ -8,4 +10,20 @@ const AIRTABLE_BASE = process.env.REACT_APP_AIRTABLE_BASE;
 const URL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/HARRY%20POTTER`;
 
 
-export default function TriviaData() {}
+export default function TriviaData() {
+  const [questions, setQuestions] = useState({});
+  
+  useEffect(() => {
+    fetchQuestions();
+  }, []);
+  
+  const fetchQuestions= async () => {
+    const questionsURL = `${URL}/${id}`;
+    const res = await axios.get(questionsURL, {
+      headers: {
+        Authorization: `Bearer ${AIRTABLE_KEY}`,
+      },
+    });
+    setQuestions(res.data);
+  };
+}
